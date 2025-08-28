8b58efec50378ef814fb032b45e080cdd498f911
Feruz <157601036+feruzida@users.noreply.github.com>
2025-08-28 17:29:33 +0500

    Update Main.java

--- two_pointers/Main.java
+++ two_pointers/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1, 1, 1, 2, 2, 3, 3, 3, 3};
+        int[] arr = {1,1, 1, 2, 2, 3, 3, 3, 3};
         int n = arr.length;
         int index = 0;
 
