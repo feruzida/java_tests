8845ecae06cf4fe181f3a4a76f9dbf11660e4295
Velia Yronwood <veliayro@te-d7.tash.21-school.uz>
2025-08-28 17:26:27 +0500

    4 test

---
+++ two_pointers/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 2, 2, 3, 3, 3, 3};
+        int n = arr.length;
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || arr[i] != arr[i - 1]) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+        for (int i = 0; i < index; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
